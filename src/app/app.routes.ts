dd97fc299067ced152e0ab22260d1151e017bbe3
Hemant kumar shakyawal <hemantkumarshakyawal@gmail.com>
2026-02-13 14:16:15 +0530

    category ,login added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Layout } from './shared/components/layout/layout';
+import { Dashboard } from './components/admin/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+
+    {
+        path: '',
+        component: Layout,
+        children: [
+            {
+                path: '',
+                loadComponent: () =>
+                    import('./components/home/home').then(c => c.Home),
+            },
+            {
+                path: 'home',
+                loadComponent: () =>
+                    import('./components/home/home').then(c => c.Home),
+            }
+        ],
+    },
+
+    {
+        path: 'dashboard',
+        component: Dashboard,
+        children: [
+            {
+                path: 'add-category',
+                loadComponent: () =>
+                    import('./components/admin/add-category/add-category')
+                        .then(c => c.AddCategory),
+            },
+        ],
+    },
+];
